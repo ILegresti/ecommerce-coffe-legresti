0e9f2b449d1700f108412bc3e087b1f634a37311
ignacio <ignaciolegresti2@gmail.com>
2023-08-21 21:04:37 -0300

    primer commit

---
+++ mi-proyecto-react/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+/* import './App.css' */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Navbar/navbar';
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ mi-proyecto-react/vite-project/src/Navbar/navbar.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function ColorSchemesExample() {
+  return (
+    <>
+        <header>
+        <Navbar expand='lg' bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Coffe name</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Productos</Nav.Link>
+            <Nav.Link href="#pricing">informacion</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+        </header>
+    </> 
+);
+}
+
+export default ColorSchemesExample;
